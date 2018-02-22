32022045aefed2a7a4ba0c5df8b3667a2873c018
Claudia marti <claudia@byod-110-101.pub.uniarts.fi>
2018-02-22 12:44:41 +0200

    added if statements

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -10,7 +10,7 @@ oriented = function(e) {
 
                 //north
          if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = "nothing" "enter" "nothing";
+                text_area.textContent = "nothing <br/> nothing";
         }
                 //east
         if(compassHeading >= 45 && compassHeading < 135) {
