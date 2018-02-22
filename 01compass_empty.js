b81f6fa880bc891e6c0164febb3cb91531d67f23
Claudia marti <claudia@byod-110-101.pub.uniarts.fi>
2018-02-22 12:15:53 +0200

    added if statements

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -9,19 +9,19 @@ oriented = function(e) {
         }
 
          if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = compassHeading;
+                text_area.textContent = "nothing";
         }
 
         if(compassHeading >= 45 && compassHeading < 135) {
-                text_area.textContent = "east 5";
+                text_area.textContent = "nothing";
         }
 
         if(compassHeading >= 135 && compassHeading < 225) {
-                text_area.textContent = "south 5";
+                text_area.textContent = "nothing ";
         }
 
         if(compassHeading >= 225 && compassHeading < 315) {
-                text_area.textContent = "west 5";
+                text_area.textContent = "table";
         }
         
          //text_area.textContent = compassHeading;
